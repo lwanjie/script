c502d1910a79d8d1cda1bda59dfd25c2a9d67fb9
lwanjie <79705243+lwanjie@users.noreply.github.com>
2025-04-21 23:44:44 +0800

    Delete neb/grepdata.sh

--- neb/grepdata.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-grep "VBM (" run.out >> out.dat
-grep "CBM (" run.out >> out.dat
-grep "Vacuum-Level (" run.out >> out.dat
-grep "energy  without entropy" mobility-x/*/scf/OUTCAR >> out.dat
-#grep "entropy=" mobility-y/*/scf/OUTCAR >> out.dat
